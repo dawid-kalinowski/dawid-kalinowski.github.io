cd5bafaa31c4a25226477929f3c06947eb886e9a
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-04-11 23:56:34 +0200

    easter egg

---
+++ note_master/licznik/script.js
@@ -0,0 +1,28 @@
+
+        var targetDate = new Date("February 17, 2025 00:00:00").getTime();
+
+
+        var x = setInterval(function() {
+
+
+            var now = new Date().getTime();
+            
+
+            var distance = targetDate - now;
+            
+
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+
+            document.getElementById("countdown").innerHTML = "Czas, żeby znaleźć robotę: " + days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+            
+
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML = "Czas minął!";
+            }
+        }, 1000);
